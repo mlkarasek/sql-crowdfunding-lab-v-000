7df55c4cdaa3ce909149bc0710ba0cfd8f356159
Monica  Karasek <mlkarasek@gmail.com>
2018-07-29 21:55:02 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,6 +1,6 @@
-CREATE TABLE projects 
+CREATE TABLE projects
 (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);
-CREATE TABLE users 
+CREATE TABLE users
 (id INTEGER PRIMARY KEY, name TEXT,  age INTEGER);
-CREATE TABLE pledges 
+CREATE TABLE pledges
 (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
